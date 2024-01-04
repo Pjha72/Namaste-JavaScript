19ed2206156d7178c57895cd2933d843f84085ca
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2024-01-04 12:35:02 +0530

    Update index.js

--- Ep-24 async await/index.js
+++ Ep-24 async await/index.js
@@ -18,52 +18,52 @@
     * 7. Async await Vs Promise.then/.catch
 **/
 
-// const p = new Promise((resolve, reject)=>{
-//     setTimeout(()=>{
-//         resolve("Promise Resolved Value");
-//     },20000);
-// });
+const p = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise Resolved Value");
+    },20000);
+});
 
-// const p1 = new Promise((resolve, reject)=>{
-//     setTimeout(()=>{
-//         resolve("Promise Resolved Value!!");
-//     },40000);
-// });
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve("Promise Resolved Value!!");
+    },40000);
+});
 
 // **Create a async function : This async function will**
 // **always return a promise**
-// async function getData(){
-//     return p;
-// }
+async function getData(){
+    return p;
+}
 
-// const dataPromise = getData();
-// console.log(data); // Promise { 10 }
-// dataPromise.then((res) => console.log(res)); // Namasate
+const dataPromise = getData();
+console.log(data); // Promise { 10 }
+dataPromise.then((res) => console.log(res)); // Namasate
 
 // **How to handle promise before using async await**
-// function getData(){
-//     // JS Engine will not wait for the promise to resolve
-//     p.then((res) =>console.log(res));
-//     console.log("Namaste");
-// }
+function getData(){
+    // JS Engine will not wait for the promise to resolve
+    p.then((res) =>console.log(res));
+    console.log("Namaste");
+}
 
-// getData();
+getData();
 
 // **Handle promise Using await with async function**
-// async function handlePromise(){
-//     console.log("Javascript");
+async function handlePromise(){
+    console.log("Javascript");
 
-//     // JS Engine was waiting for the promise to resolved
-//     const data = await p;
-//     console.log("Namaste");
-//     console.log(data);
+    // JS Engine was waiting for the promise to resolved
+    const data = await p;
+    console.log("Namaste");
+    console.log(data);
 
 
-//     const data1 = await p1;
-//     console.log("Namaste 1");
-//     console.log(data1);
-// }
-// handlePromise();
+    const data1 = await p1;
+    console.log("Namaste 1");
+    console.log(data1);
+}
+handlePromise();
 
 // **What happens behind the scences**
 // handlePromise() : it will suspend when it will find the await
@@ -75,20 +75,20 @@
 const API_URL = "https://api.github.com/users/Pjha72";
 
 // **Real world example of async/await**
-// async function getUser(){
-//     const data = await fetch(API_URL);
+async function getUser(){
+    const data = await fetch(API_URL);
 
-//     // fetch() => Response object is a readable string i.e
-//     // .json() is a promise => gives a json value
+    // fetch() => Response object is a readable string i.e
+    // .json() is a promise => gives a json value
 
-//     const jsonValue = await data.json();
+    const jsonValue = await data.json();
 
-//     // Normal way to write
-//     // fetch(API_URL).then((res) => res.json()).then(res=>console.log(res));
+    // Normal way to write
+    // fetch(API_URL).then((res) => res.json()).then(res=>console.log(res));
 
-//     console.log(jsonValue);
-// }
-// getUser();
+    console.log(jsonValue);
+}
+getUser();
 
 // ** How to Handle Error in async/await **
 async function getUser(){
@@ -111,7 +111,7 @@ async function getUser(){
 }
 getUser();
 // Traditional way to handle error
-// getUser().catch((err) => console.log(err));
+getUser().catch((err) => console.log(err));
 
 // ** Interview Tips **
 /**
